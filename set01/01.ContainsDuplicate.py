670a60c11cdb47adf3f993c7bead369346f72372
ishiekh-dev <ishiekh.dev.v1@gmail.com>
2022-09-12 02:51:35 +0200

    find if the array has duplicated number in it or not

--- set01/01.ContainsDuplicate.py
+++ set01/01.ContainsDuplicate.py
@@ -1,18 +1,26 @@
+"""
+
+https://leetcode.com/problems/contains-duplicate/
+
+check if the given array has Duplicates or not
+"""
 class Solution(object):
     def containsDuplicate(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
+
+        # create hashset to store each element in array while traverse
         hashset = set()
-
+        # for each element in array
         for n in nums:
-
+            # check if the element exist in hashset , if it's then it's duplicate
             if n in hashset:
                 return True
-
+            # else add it to hashset
             hashset.add(n)
-
+        # no duplication return false
         return False
 
 
